b22d48a7953a6e40c23e8a6528c47363d3b95069
Adrià Munuera Borrell <adriamunuera@me.com>
2022-12-03 19:25:58 +0100

    First two days answers in Python. Wanted to do them in PROLOG but...

---
+++ day_two/part_one.py
@@ -0,0 +1,78 @@
+from enum import Enum
+
+class Election(Enum):
+	ROCK = 1
+	PAPER = 2
+	SCISSORS = 3
+
+def convertToElection(letter):
+	if letter == 'A' or letter == 'X':
+		return Election.ROCK
+	elif letter == 'B' or letter == 'Y':
+		return Election.PAPER
+	elif letter == 'C' or letter == 'Z':
+		return Election.SCISSORS
+	return Election.SCISSORS
+	
+class MatchResult(Enum):
+	LOSE = 0
+	DRAW = 1
+	WIN = 2
+
+def checkIfWin(oponent_election, your_election):
+	if oponent_election == your_election:
+		return MatchResult.DRAW
+
+	if your_election == Election.ROCK:
+		if oponent_election == Election.SCISSORS:
+			return MatchResult.WIN
+		else:
+			return MatchResult.LOSE
+
+	if your_election == Election.PAPER:
+		if oponent_election == Election.ROCK:
+			return MatchResult.WIN
+		else:
+			return MatchResult.LOSE
+
+	if your_election == Election.SCISSORS:
+		if oponent_election == Election.PAPER:
+			return MatchResult.WIN
+		else:
+			return MatchResult.LOSE
+
+def checkMatch(oponent_election, your_election):
+	#print(your_election, oponent_election)
+	oponent_election = convertToElection(oponent_election)
+	your_election = convertToElection(your_election)
+	# math trick...
+	#print(your_election, oponent_election)
+	return your_election.value + 3*(checkIfWin(oponent_election, your_election).value)
+
+def readInput(file='testInput'):
+	with open(file) as f:
+		lines = f.readlines()
+		total_score = 0
+		for l in lines:
+			total_score = total_score + checkMatch(l[0], l[2])
+		return total_score
+
+print(readInput(file='input'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
